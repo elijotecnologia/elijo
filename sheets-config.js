8787e7e7d862c7f683d19e0ecfa036b20176906f
elijotecnologia <elijotecnologia@gmail.com>
2026-07-20 11:26:36 -0500

    Update sheets-config.js

--- sheets-config.js
+++ sheets-config.js
@@ -24,7 +24,7 @@
 
 const SHEETS_CONFIG = {
   // ► PEGA AQUÍ TU URL DE GOOGLE SHEETS CSV ◄
-  SHEETS_CSV_URL: 'https://docs.google.com/spreadsheets/d/1NyN3ww79H0TB1Cpi8woqFgm9ZmptG6_ECO_sb27eCiw/edit?usp=sharing',   // Ejemplo: 'https://docs.google.com/spreadsheets/d/TU_ID/export?format=csv&gid=0'
+  SHEETS_CSV_URL: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRa0B1xjo18VL2DjYLxQtOWVyvDMp4lDXEUkY09htxYcw_lvbwaQVED4Chx83vG26SLssN-VkVl9gYP/pub?gid=0&single=true&output=csv',   // Ejemplo: 'https://docs.google.com/spreadsheets/d/TU_ID/export?format=csv&gid=0'
 
   // Número de WhatsApp (con código de país, sin +)
   WHATSAPP_NUMBER: '51918394348',
